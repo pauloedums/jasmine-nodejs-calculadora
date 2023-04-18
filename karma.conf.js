2516c7f4aa9198d7feb0878b0060ff85a2c55628
Paulo Marques <pauloedums@gmail.com>
2023-04-17 22:00:58 -0300

    Circle Ci config

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     concurrency: Infinity
   })
