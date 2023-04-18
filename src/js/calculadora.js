94317d715a4a7c40ac7286d4ff4537b56fb31f46
Paulo Marques <pauloedums@gmail.com>
2023-04-17 23:06:17 -0300

    add sum implementation

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,6 +1,10 @@
 var Calculadora = {
     adicionar: (num1, num2) => {
-      return num1 + num2;
+        if(isNaN(num1) || isNaN(num2)) return 0;
+
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        return num1 + num2;
     },
     subtrair: (num1, num2) => {
         return num1 - num2;
